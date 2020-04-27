ae8cc0e004181d512989688472a8e0d50a481c0b
Jasper <zipdevil@gmail.com>
2020-04-27 11:56:23 -0700

    add comments routes/controler/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+    # get    '/comments',          to: 'comments#index', as: 'comments'
+    # post   '/comments',          to: 'comments#create'
+    # get    '/comments/new',      to: 'comments#new',   as: 'new_comments'
+    # get    '/comments/:id',      to: 'comments#show',  as: 'post_comment'
+    # get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comments'
+    # patch  '/comments/:id',      to: 'comments#update'
+    # delete '/comments/:id',      to: 'comments#destroy'
+  end
+
   resources :pages
-  # get '/pages', to: 'pages#index'
-  # post '/pages', to: 'pages#create'
-  # get '/pages/new', to: 'pages#new', as: 'new_page'
-  # get '/pages/:id', to: 'pages#show', as: 'page'
-  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  # patch '/pages/:id', to: 'pages#update'
-  # delete '/pages/:id', to 'pages#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
